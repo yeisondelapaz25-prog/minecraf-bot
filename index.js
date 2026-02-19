8d6f3806f1fbd68483f88a8d4469ffcb2c7f885c
yeisondelapaz25-prog <yeisondelapaz25@gmail.com>
2026-02-19 08:22:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,19 @@ const bot = mineflayer.createBot({
   host: '195.201.153.149',
   port: 22214,
   username: 'yeisonlive'
-{)
-bot.on('spawn', ()  =>  {
+})
+
+bot.on('spawn', () => {
   console.log('Bot conectado correctamente')
 })
-bot.on('end', ()  => {
-  console.log('Bot desconectado reconectando...')
-  settimeout(() => {
-    bot.conect()
+
+bot.on('end', () => {
+  console.log('Bot desconectado, reconectando...')
+  setTimeout(() => {
+    bot.connect()
   }, 5000)
 })
-  bot.on('error', (err)  => {
-    console.log('error:',err)
-  })
+
+bot.on('error', (err) => {
+  console.log('error:', err)
+})
